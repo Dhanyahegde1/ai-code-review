35f14f84b4eb459187a99674f1ecd3220d2d0801
Dhanya Hegde <dhanyahegde1759@gmail.com>
2026-03-03 22:28:19 +0530

    add main.py for swagger testing

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from config import get_settings
+from pydantic import BaseModel
+from backend.services.code_analyzer import analyze_code
+from backend.services.ai_engine import rule_based_ai
 
 settings = get_settings()
 
@@ -7,6 +10,14 @@ app = FastAPI(
     title=settings.app_name,
     version=settings.app_version
 )
+class CodeRequest(BaseModel):
+    code: str
+
+@app.post("/analyze")
+def analyze_endpoint(request: CodeRequest):
+    result = analyze_code(request.code)
+    suggestions = rule_based_ai(result)
+    return {"analysis": result, "suggestions": suggestions}
 
 @app.get("/health")
 async def health_check():
